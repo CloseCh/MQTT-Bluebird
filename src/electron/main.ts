a25a994b71ec9c694615aa041fb97a5441e3d695
Close <github.laundry472@passinbox.com>
2025-09-16 14:28:58 +0200

    Fix for different SO

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,13 +2,31 @@ import {app, BrowserWindow} from 'electron';
 import path from 'path';
 import { isDev } from './until.js';
 
-type test = string;
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
   if (isDev()){
     mainWindow.loadURL('http://localhost:5123/');
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+  }
+}
+
+app.on('ready', () => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
 });
