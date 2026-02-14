9146eba2149ebea386455f6ebca3d0572371319a
Close <github.laundry472@passinbox.com>
2026-02-14 23:07:25 +0100

    fix connexion to brocker

--- app/src/electron/MQTTsubscriptor.ts
+++ app/src/electron/MQTTsubscriptor.ts
@@ -2,21 +2,33 @@ import mqtt from 'mqtt';
 import { BrowserWindow } from 'electron';
 import { ipcWebContentsSend } from './util/until.js';
 
-const client = mqtt.connect('mqtt://localhost:1883');
+const client = mqtt.connect('mqtt://localhost:1883', {
+  clientId: 'mqtt_client_' + Math.random().toString(16).substring(2, 8),
+  clean: true,
+  connectTimeout: 4000,
+  reconnectPeriod: 1000,
+});
 
-export function MQTTmessage(mainWindow: BrowserWindow){
-  client.on('message', (topic,message) => {
-    const strigMenssage = message.toString();
-    const numberMessage = Number(strigMenssage);
+export function MQTTmessage(mainWindow: BrowserWindow) {
+  client.on('message', (topic, data) => {
+    const text = data.toString();
+
+    let parsed: unknown;
+
+    try {
+      parsed = JSON.parse(text);
+    } catch {
+      parsed = text; // no es JSON → lo dejamos como string
+    }
+    console.log(parsed)
     ipcWebContentsSend('message', mainWindow.webContents, {
       topic,
-      numberMessage,
+      data: parsed
     });
   });
 }
 
 client.on('connect', () => {
   console.log('Conexion establecida');
-  client.subscribe('casa/habitacion1/temperatura');
+  client.subscribe('#');
 });
-
--- app/types.d.ts
+++ app/types.d.ts
@@ -1,27 +1,12 @@
 //Global types
-type Statistics = {
-  cpuUsage: number;
-  ramUsage: number; 
-  storageUsage: number;
-};
-
-type StaticData = {
-  totalStorage: number;
-  cpuModel: string;
-  totalMemoryGB: number;
-};
-
 type MQTTmessage = {
-  topic: string;
-  numberMessage: number;
+  topic: string,
+  data: unknown;
 };
 
 type EventPayloadMapping = {
-  statistics: Statistics;
-  getStaticData: StaticData;
   message: MQTTmessage;
 };
-
 
 type UnsubscribeFunction = () => void;
 
