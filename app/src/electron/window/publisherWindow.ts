b5df399e62d68ca10a34246bb368303376c0b3c7
Close <github.laundry472@passinbox.com>
2026-03-28 20:00:22 +0100

    Add publisherWindow creation

---
+++ app/src/electron/window/publisherWindow.ts
@@ -0,0 +1,25 @@
+import { BrowserWindow } from "electron";
+import { getPreloadPath, getUIPath } from "../util/pathResolver.js";
+import { isDev } from "../util/until.js";
+import { setupPublisher } from "../services/MQTTPublisher.js";
+
+function publisherWindow(parent?: BrowserWindow) {
+  const publisherWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+    width: 800,
+    height: 600,
+    parent: parent
+  });
+
+  if (isDev()) {
+    publisherWindow.loadURL('http://localhost:5123/publish'); 
+  } else {
+    publisherWindow.loadFile(getUIPath(), {hash: '/publish'});
+  }
+
+  setupPublisher();
+}
+
+export default publisherWindow;
