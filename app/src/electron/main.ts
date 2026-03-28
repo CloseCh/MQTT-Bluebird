5d1fa7aa3ab229d199d5b84e4512a0368789fe12
Close <github.laundry472@passinbox.com>
2026-03-28 20:00:13 +0100

    Separed mainWindow creation

--- app/src/electron/main.ts
+++ app/src/electron/main.ts
@@ -1,36 +1,13 @@
-import { app, BrowserWindow} from 'electron';
-import { isDev } from './util/until.js';
-import { getPreloadPath, getUIPath } from './util/pathResolver.js';
-import { setupSubscriptor } from './services/MQTTSubscriptor.js';
+import { app, BrowserWindow } from 'electron';
 import { destroyClient } from './services/MQTTConnection.js';
-import { setupPublisher } from './services/MQTTPublisher.js';
-
-const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    webPreferences: {
-      preload: getPreloadPath(),
-    },
-    width: 1200,
-    height: 800
-  });
-
-  if (isDev()){
-    mainWindow.loadURL('http://localhost:5123/');
-  } else {
-    mainWindow.loadFile(getUIPath());
-  }
-
-  // Envio de datos
-  setupSubscriptor(mainWindow);
-  setupPublisher();
-};
+import createMainWindow from './window/mainWindow.js';
 
 app.on('ready', () => {
-  createWindow()
+  createMainWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createMainWindow();
     }
   });
 });
@@ -40,6 +17,5 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-
   destroyClient();
 });
---
+++ app/src/electron/window/mainWindow.ts
@@ -0,0 +1,24 @@
+import { BrowserWindow } from "electron";
+import { getPreloadPath, getUIPath } from "../util/pathResolver.js";
+import { isDev } from "../util/until.js";
+import { setupSubscriptor } from "../services/MQTTSubscriptor.js";
+
+function createMainWindow () {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+    width: 1200,
+    height: 800
+  });
+
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5123/');
+  } else {
+    mainWindow.loadFile(getUIPath());
+  }
+
+  setupSubscriptor(mainWindow);
+}
+
+export default createMainWindow;
