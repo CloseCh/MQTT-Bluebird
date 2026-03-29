7b0cb7639d0c1518f52ff18f1f35426057e65472
Close <github.laundry472@passinbox.com>
2026-03-29 17:07:41 +0200

    Add header to publish layout

--- app/src/ui/layout/PublishLayout/PublishLayout.tsx
+++ app/src/ui/layout/PublishLayout/PublishLayout.tsx
@@ -1,11 +1,15 @@
-import { CssBaseline } from "@mui/material";
+import Header from "@/components/Header/Header";
+import { CssBaseline, Stack } from "@mui/material";
 import { Outlet } from "react-router";
 
 export default function PublishLayout() {
   return (
     <>
       <CssBaseline/>
-      <Outlet/>
+      <Stack sx={{ height: '100vh', overflow: 'hidden' }}>
+        <Header title='Publish message' />
+        <Outlet />
+      </Stack>
     </>
   );
 }
