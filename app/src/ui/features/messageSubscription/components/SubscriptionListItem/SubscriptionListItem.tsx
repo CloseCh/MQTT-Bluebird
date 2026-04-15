b31c2f00badf2fad4a58a853bbfbfe4bcf42561c
Close <github.laundry472@passinbox.com>
2026-04-15 16:20:24 +0200

    Add a delete topic

--- app/src/ui/features/messageSubscription/components/SubscriptionListItem/SubscriptionListItem.tsx
+++ app/src/ui/features/messageSubscription/components/SubscriptionListItem/SubscriptionListItem.tsx
@@ -1,4 +1,7 @@
-import { Checkbox, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Checkbox, IconButton, ListItem, ListItemText } from "@mui/material";
+import { useSubscriptionContext } from "../../hooks/useSubscriptionContext";
+
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 interface Prop {
   labelId: string;
@@ -9,23 +12,31 @@ interface Prop {
 }
 
 export function SubscriptionListItem({ labelId, index, value, checked, handleToggle }: Prop) {
+
+  const { unsubscribe } = useSubscriptionContext();
+
+  function handleClick(value: string) {
+    unsubscribe([value]);
+  }
+
   return (
     <ListItem
       key={value}
-      disablePadding
+      sx={{
+        pl: "10px"
+      }}
     >
-      <ListItemButton role={undefined} onClick={() => handleToggle(index)} dense>
-        <ListItemIcon>
-          <Checkbox
-            edge="start"
-            checked={checked.includes(index)}
-            tabIndex={-1}
-            disableRipple
-            slotProps={{ input: { 'aria-labelledby': labelId } }}
-          />
-        </ListItemIcon>
-        <ListItemText id={labelId} primary={`${value}`} />
-      </ListItemButton>
+      <Checkbox
+        edge="start"
+        checked={checked.includes(index)}
+        onClick={() => handleToggle(index)}
+        tabIndex={-1}
+        slotProps={{ input: { 'aria-labelledby': labelId } }}
+      />
+      <ListItemText id={labelId} primary={`${value}`} />
+      <IconButton type="button" aria-label="Add" onClick={() => handleClick(value)}>
+        <DeleteOutlineIcon />
+      </IconButton>
     </ListItem>
   );
 }
