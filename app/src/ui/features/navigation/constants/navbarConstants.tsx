64e32f70d772a577289ff561d26765cd15e2b11b
Close <github.laundry472@passinbox.com>
2026-05-04 22:27:41 +0200

    Change Table Config icons

--- app/src/ui/features/navigation/constants/navbarConstants.tsx
+++ app/src/ui/features/navigation/constants/navbarConstants.tsx
@@ -1,4 +1,4 @@
-import { Dashboard, Send, RssFeed } from "@mui/icons-material";
+import { Topic , History, Timeline, Send, RssFeed } from "@mui/icons-material";
 import type { NavItemConfig } from "../types/navigation.types";
 
 export const DRAWER_WIDTH = 60;
@@ -17,7 +17,7 @@ export const OVERLAY_IDS = {
 export const NAV_ITEMS_CONFIG: NavItemConfig[] = [
   { id: 1, label: "subcription",    icon: <RssFeed sx={style} />,   overlayId: OVERLAY_IDS.NAV_SUBSCRIPTION },
   { id: 2, label: "publish",        icon: <Send sx={style} />,      overlayId: OVERLAY_IDS.NAV_PUBLISH, dividerAfter: true },
-  { id: 3, label: "history",        icon: <Dashboard sx={style} />, overlayId: OVERLAY_IDS.TABLE_HISTORY },
-  { id: 4, label: "topic",          icon: <Dashboard sx={style} />, overlayId: OVERLAY_IDS.TABLE_TOPIC },
-  { id: 5, label: "last",           icon: <Dashboard sx={style} />, overlayId: OVERLAY_IDS.TABLE_LAST },
+  { id: 3, label: "history",        icon: <History sx={style} />, overlayId: OVERLAY_IDS.TABLE_HISTORY },
+  { id: 4, label: "topic",          icon: <Topic sx={style} />, overlayId: OVERLAY_IDS.TABLE_TOPIC },
+  { id: 5, label: "last",           icon: <Timeline sx={style} />, overlayId: OVERLAY_IDS.TABLE_LAST },
 ];
